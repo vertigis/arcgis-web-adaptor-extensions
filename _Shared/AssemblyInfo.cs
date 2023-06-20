d0f252c5d18800feca60361f3322ecd2e94dccee
Ryan Cooney <ryan.cooney@vertigis.com>
2023-06-19 18:57:10 -0700

    fix: set version in assembly info

---
+++ _Shared/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("VertiGIS North America Ltd.")]
+[assembly: AssemblyCopyright("Copyright © 2023 VertiGIS North America Ltd. All Rights Reserved.")]
+[assembly: AssemblyProduct("VertiGIS Studio Web Adaptor Extensions")]
